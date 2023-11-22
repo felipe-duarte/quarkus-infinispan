2312843af57241d30a02eb82d31c17f73118776f
felipe-duarte <felipe.duarte@gmail.com>
2023-11-22 10:35:46 -0300

    upload quickstart infinispan

---
+++ src/java/org/acme/infinispan/client/Greeting.java
@@ -0,0 +1,15 @@
+package org.acme.infinispan.client;
+
+import org.infinispan.protostream.annotations.ProtoField;
+
+public class Greeting {
+   @ProtoField(number = 1)
+   public String name;
+
+   @ProtoField(number = 2)
+   public String message;
+   
+   public Greeting() {
+	   super();
+   }
+}
